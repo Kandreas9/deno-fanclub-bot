e9b0a813e0f3e939ea3f272f1282f8ef83b83695
Kandreas9 <andkarido@gmail.com>
2021-09-08 18:28:25 +0200

    feat(commands): 📎 added format command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,15 +2,19 @@ import { CommandContext } from "../../deps.ts";
 
 export const commands = [
 	{
-		name: "ping",
+		name: "format",
 		onExecute(ctx: CommandContext) {
-			ctx.message.reply("Pong!");
+			ctx.channel.send(
+				"You can format code on discord like so: \n> \\`\\`\\`typescript \n> function deno(node: any) { \n>   return node.split().sort().join(); \n> } \n> \\`\\`\\`"
+			);
 		},
 	},
 	{
 		name: "deno",
 		onExecute(ctx: CommandContext) {
-			ctx.message.reply("Deno is better than Node!");
+			ctx.message.reply(
+				"Deno is a secure javascript/typescript runtime built on rust!"
+			);
 		},
 	},
 ];
